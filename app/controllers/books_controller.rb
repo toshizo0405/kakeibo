b2e71e36641af91d534fd63a270cbffc32791790
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-07-05 12:56:42 +0000

    [Update3]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,9 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/books", to: "books#index"
+  get "/books/:id",  to: "books#show", as: "book"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
