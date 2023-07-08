3cea2ed4384655d5ced14084a6652f9f58d55a50
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-07-08 14:50:39 +0000

    [Update5]

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   post "/books" , to: "books#create"
   get "/books/new", to: "books#new", as: "new_book"
   get "/books/:id",  to: "books#show", as: "book"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
