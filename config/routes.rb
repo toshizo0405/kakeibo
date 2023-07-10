da32393aa829aad413e35d774154366a10dbec59
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-07-10 14:06:31 +0000

    [Update8]

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "/books/:id",  to: "books#show", as: "book"
   patch "/books/:id", to: "books#update"
   delete "/books/:id", to: "books#destroy"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
