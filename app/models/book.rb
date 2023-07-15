5225f0dec5f12b694117ac7ad520418745d98dc4
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-07-15 13:58:11 +0000

    [Update12]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,9 @@
 class Book < ApplicationRecord
-  validates :year, presence: true
-  validates :month, presence: true
-  validates :inout, presence: true
-  validates :category, presence: true
-  validates :amount, presence: true
+
+validates :year, presence: true
+validates :month, presence: true
+validates :inout, presence: true
+validates :category, presence: true
+validates :amount, presence: true
+
 end
