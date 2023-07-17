b2bc4be754b414004051d9c37e460526de1837d0
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-07-17 07:15:53 +0000

    [Update18]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,8 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @books = @books.where(year: params[:year]) if params[:year].present?
+    @books = @books.where(month: params[:month]) if params[:month].present?
   end
 
   def show
