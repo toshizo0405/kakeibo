cdd3297873b99a25da70c448ec30d8468f25563b
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-08-26 14:20:24 +0000

    [Update26]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
 
   def create
     user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
