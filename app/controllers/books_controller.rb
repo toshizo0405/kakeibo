002026036e8d19bb5f62adbbe6fd4aabcf4e55cc
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-09-07 14:38:41 +0000

    [Update31]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,6 @@ class BooksController < ApplicationController
 
   before_action :set_book, only: [:show, :edit, :update, :destroy]
   
-
   def index
     @books = Book.all
     @books = @books.where(year: params[:year]) if params[:year].present?
